24681b4035307459e753d30c01dbf1f9dfb76437
Ameya Potdar <69340181+redfieldC@users.noreply.github.com>
2023-10-30 18:41:37 +0530

    Update urls.py

--- django/Productstore/store/storeapp/urls.py
+++ django/Productstore/store/storeapp/urls.py
@@ -1,21 +1,33 @@
-from django.urls import path,include
-from storeapp import views 
+from django.urls import path
+from ash_store import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
-    path('',views.home),
     path('home',views.homepage),
+    path('about',views.about),
     path('contact',views.contact),
     path('edit/<id>',views.edit),
     path('delete/<id>',views.delete),
     path('addition/<x>/<y>',views.addition),
-    path('catfilter/<int:catv>',views.cat_filter),
-    path('pricefilter/',views.price_filter),
+
+    path('',views.home),
+    path('aboutus',views.about_us),
+    path('contactus',views.contact_us),
+    path('cart',views.cart),
+    path('porder',views.placeorder),
+    path('catfilter/<cid>',views.cat_filter),
+    path('pricerange',views.pricerange),
     path('sort',views.sort),
     path('search',views.search),
-    path('removecart/<cid>',views.remove_cart),
+
     path('pdetails/<pid>',views.pdetails),
-    path('porder',views.placeorder),
     path('cart/<prod_id>',views.add_to_cart),
     path('removeitem/<rid>',views.remove_item),
-    path('cart',views.cart_page)
+    path('changeqty/<cqid>',views.changeqty),
+    
 ]
+
+if settings.DEBUG:
+
+    urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
